d1c824e0748cdedb203b9fe51d849904b7c81600
HippiHo <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>
2024-07-25 16:53:38 +0200

    setup

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,59 @@
+import { View, Pressable, Text, StyleSheet, Platform } from "react-native";
+
+export const CategoryGridTile = ({ title, color }) => {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    // shadow for Android
+    elevation: 4,
+    // shadow for IOS
+    // for shadow to have an effect it needs a background color
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.5,
+    shadowOffset: {
+      width: 1,
+      height: 3,
+    },
+    shadowRadius: 8,
+    // Android: for the ripple effect
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.75,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
